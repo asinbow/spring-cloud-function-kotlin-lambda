f6edde3411ac018d13661311181ba34109c1bb03
Hao Wang <hao.wang@flexport.com>
2022-08-08 09:44:07 +0800

    init

---
+++ src/test/kotlin/com/springcloudfunction/kotlinlambda/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.springcloudfunction.kotlinlambda.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
